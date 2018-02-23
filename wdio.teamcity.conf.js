db4ea5631bf1d9e140827886eb5e4b1fd2fd9708
Andrei Dzeichyk <andreidei4ik@gmail.com>
2018-02-24 00:02:20 +0300

    Change binary chrome

--- wdio.teamcity.conf.js
+++ wdio.teamcity.conf.js
@@ -11,9 +11,10 @@ exports.config = {
         maxInstances: 5,
         browserName: 'chrome',
         chromeOptions: {
-            args: ['--headless'],
-            binary: ['c:/Users/andre/AppData/Local/Google/Chrome/Application/chrome.exe']
-        }}
+            args: ['--headless']
+        },
+        binary: ['c:/Users/andre/AppData/Local/Google/Chrome/Application/chrome.exe']
+    }
     ],
     sync: true,
     logLevel: 'error',
